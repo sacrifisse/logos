18936a708a01312a2e4cc7dd74c2d92bc5bb63e7
sacrifisse <ognivin300@gmail.com>
2022-04-11 13:44:31 +0300

    dd

--- js/script.js
+++ js/script.js
@@ -11,21 +11,3 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
-
-document.getElementById("foodlist")
-  .addEventListener('wheel', function(event) {
-    if (event.deltaMode == event.DOM_DELTA_PIXEL) {
-      var modifier = 0.6;
-      // иные режимы возможны в Firefox
-    } else if (event.deltaMode == event.DOM_DELTA_LINE) {
-      var modifier = parseInt(getComputedStyle(this).lineHeight);
-    } else if (event.deltaMode == event.DOM_DELTA_PAGE) {
-      var modifier = this.clientHeight;
-    }
-    if (event.deltaY != 0) {
-      // замена вертикальной прокрутки горизонтальной
-      this.scrollLeft += modifier * event.deltaY;
-      event.preventDefault();
-    }
-});
-
