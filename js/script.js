e30d683635b9dcf5394ca7e32703653322b47480
sacrifisse <79392020+sacrifisse@users.noreply.github.com>
2022-04-13 10:34:25 +0300

    finish main page

--- js/script.js
+++ js/script.js
@@ -11,3 +11,21 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+
+$(document).ready(function() { 
+  var button = $('#button-up');	
+  $(window).scroll (function () {
+    if ($(this).scrollTop () > 300) {
+      button.fadeIn();
+    } else {
+      button.fadeOut();
+    }
+});	 
+button.on('click', function(){
+$('body, html').animate({
+scrollTop: 0
+}, 800);
+return false;
+});		 
+});
